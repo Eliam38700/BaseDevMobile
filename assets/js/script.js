0faf9f1d1422eee8fbbc6361e781a463ef640cfd
Eliam Lotonga <lotongae@iut1i-605-a11.local>
2017-10-13 16:47:16 +0200

    Now we can display one character or all characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,17 +9,19 @@ function(){
 
 );
 
-$("button").click(function(){
+$(".btn1").click(function(){
 	$.getJSON( "https://swapi.co/api/people/1/?format=json", function( data ) {
 	
   	//var formData = $(data).serializeArray(); // Create array of object
   	var jsonConvertedData = JSON.stringify(data);  // Convert to json
   	console.log(data["name"]);
 
-  	$("<h2>"+data["name"]+"</h2>").appendTo(".firstName");
+  	$(".allName").html("<h2>"+data["name"]+"</h2>")
+
+  	//$("<h2>"+data["name"]+"</h2>").appendTo(".firstName");
 
 	//console.log(data);
-  $("textarea").val(jsonConvertedData);
+  //$("div.allName").val(jsonConvertedData);
   // var items = [];
   // $.each( data, function( key, val ) {
   //   items.push( "<li id='" + key + "'>" + val + "</li>" );
@@ -33,25 +35,29 @@ $("button").click(function(){
 });
 
 
-$("btn2").click(function(){
+$(".btn2").click(function(){
 	$.getJSON( "https://swapi.co/api/people/?format=json", function( data ) {
 	
   	//var formData = $(data).serializeArray(); // Create array of object
   	var jsonConvertedData = JSON.stringify(data);  // Convert to json
-  	console.log(data["name"]);
+  	//console.log(data["name"]);
 
   	//$("<h2>"+data["name"]+"</h2>").appendTo(".allName");
 
 	//console.log(data);
-  $("textarea").val(jsonConvertedData);
-  // var items = [];
-  $.each( data, function( key, val ) {
-  	if(key == "name"){
-  		items.push( "<li id='" + key + "'>" + val + "</li>" );
-  	}
+  //$("textarea").val(jsonConvertedData);
+  var items = [];
+  console.log(data["results"]);
+  $.each( data["results"], function( key, val ) {
+	  
+	  	items.push( "<li id='" + key + "'>" + val["name"] + "</li>" );
   	 
-  	}
-  });
+  	}),
+  console.log(items);
+
+  $(".allName").html("<ul>" + items.join("") + "</ul>")
+
+  //$("<ul>" + items.join("") + "</ul>").appendTo(".allName");
  
   // $( "<ul/>", {
   //   "class": "my-new-list",
