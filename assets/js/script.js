33e83b394ccd62a4d41ead2f698845e53e01ddc0
Eliam Lotonga <lotongae@iut1i-605-a11.local>
2017-10-18 09:27:39 +0200

    add an other button to display 87characters, linked main.css for display 87 characters download layzr.js librairie

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ $(".btn1").click(function(){
 	
   	//var formData = $(data).serializeArray(); // Create array of object
   	var jsonConvertedData = JSON.stringify(data);  // Convert to json
-  	console.log(data["name"]);
+  	//console.log(data["name"]);
 
   	$(".allName").html("<h2>"+data["name"]+"</h2>")
 
@@ -47,13 +47,13 @@ $(".btn2").click(function(){
 	//console.log(data);
   //$("textarea").val(jsonConvertedData);
   var items = [];
-  console.log(data["results"]);
+  //console.log(data["results"]);
   $.each( data["results"], function( key, val ) {
 	  
 	  	items.push( "<li id='" + key + "'>" + val["name"] + "</li>" );
   	 
   	}),
-  console.log(items);
+  //console.log(items);
 
   $(".allName").html("<ul>" + items.join("") + "</ul>")
 
@@ -65,3 +65,37 @@ $(".btn2").click(function(){
   // }).appendTo( "body" );
 });
 });
+  var items = [];
+
+$(".btn3").click(function(){
+  console.log('btn 3 click');
+  for (var i = 1; i <= 9; i++) {
+    console.log('requete ' + i);
+    requete(i);
+    console.log('fin requete ' + i);
+  }
+  //console.log('btn 3 apres for');
+  //console.log(items);
+  //$(".allName").html("<ul>" + items.join("") + "</ul>");
+
+  
+});
+
+function requete(i){
+  $.getJSON( "https://swapi.co/api/people/?page="+i, function( data ) {
+  
+    var jsonConvertedData = JSON.stringify(data);  // Convert to json
+    
+    //console.log(data["results"]);
+    $.each( data["results"], function( key, val ) {
+      //console.log(val);
+      items.push( "<button id='" + key + "' class= \'btn-modal\'>" + val["name"] + "</button>" );
+      
+    })
+    $(".allName").append(items);
+  });
+}
+
+
+
+$("<a>").appendTo("")
